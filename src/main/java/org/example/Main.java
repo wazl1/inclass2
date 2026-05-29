7185cc97cdb5b1d4d4b75e2ac5d326aedd07af50
Mason <masclearn@gmail.com>
2026-05-28 20:14:10 -0700

    git commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,7 @@ public class Main {
         IO.println(String.format("Hello and welcome!"));
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+            System.out.println(i);
         }
     }
 }
